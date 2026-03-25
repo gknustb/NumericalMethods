4f2d8f24df1a9b1c74fa09dccb10e61fca2f15d8
Guilherme Knust <gknust.b@gmail.com>
2026-03-25 18:00:17 -0300

    refactor: removed unused variable in ContinuousFunc, added README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# Numerical Methods example scripts
+
+Scripts used to learn Numerical Methods
+
+To-do:
+- Move to a Maven project for standardization
+
--- exampleContFunc.java
+++ exampleContFunc.java
@@ -2,7 +2,7 @@ import FunctionTools.ContinuousFunc;
 
 public class exampleContFunc {
   public static void main(String[] args) {
-    ContinuousFunc funcao = new ContinuousFunc(x -> (x*x)-5);
+    ContinuousFunc funcao = new ContinuousFunc(x -> (x * x) - 5);
     System.out.println(funcao.searchZero(-100, 100, 0.5));
   }
 }
